43041857536c13c9307de7835bf3192612491fe0
zugadon <blueshinyhead@docomo.ne.jp>
2025-11-13 12:07:18 +0900

    押したボタンをログとして出せた

--- src/debug.tsx
+++ src/debug.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GamepadUI from "./gamepadUI";
 import useController from "./gamepadAPI";
 
@@ -10,25 +10,54 @@ type GamepadState = {
 };
 
 const DebugUI: React.FC<GamepadState> = ({ id, buttons, axes }) => {
+
+    const [preButtons, setPreButtons] = useState<readonly GamepadButton[]>(buttons);
+    const [log, setLog] = useState<number[]>([]);
+    useEffect(() => {
+        for (let i = 0; i < buttons.length; i++) {
+            if (buttons[i].pressed && !preButtons[i].pressed) {
+                setLog(prev => {
+                    const updated = [i,...prev];
+                    return updated.slice(0,20);
+                });
+            }
+        }
+        setPreButtons(buttons);
+    }, [buttons]);
+
     return (
         <div>
-            <h2>{"id:" + id}</h2>
-            <div>
-                <h3>ボタン</h3>
-                {buttons.map((btn, i) => (
-                    <div key={i}>
-                        ボタン {i}: {btn.pressed ? "押下中" : "離されている"} (値:{" "}
-                        {btn.value})
+            <h2>{"id: " + id}</h2>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div>
+                    <div>
+                        <h3>ボタン</h3>
+                        {buttons.map((btn, i) => (
+                            <div key={i}>
+                                ボタン {i}: {btn.pressed ? "押下中" : "離されている"} (値:{" "}
+                                {btn.value})
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <div>
-                <h3>スティック</h3>
-                {axes.map((axis, i) => (
-                    <div key={i}>
-                        軸 {i}: {axis.toFixed(2)}
+                    <div>
+                        <h3>スティック</h3>
+                        {axes.map((axis, i) => (
+                            <div key={i}>
+                                軸 {i}: {axis.toFixed(2)}
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
+                <div style={{ marginLeft: "50px" }}>
+                    <h2>ボタンログ(最新順)</h2>
+                    {log.map((btnIndex, i) => (
+                        <div key={i} style={{border: "2px solid black",
+                            display: "flex",justifyContent: "center",alignItems: "center",
+                            aspectRatio: "1",borderRadius: "50%",backgroundColor: "lightgray",
+                            color: "gray",width:"30px"
+                        }}>{btnIndex}</div>
+                    ))}
+                </div>
             </div>
         </div>
     );
@@ -41,7 +70,7 @@ const Main = () => {
     const Controller = useController();
 
     return (
-        <div style={{}}>
+        <div>
             {Controller ? (
                 <div style={{ display: "flex", justifyContent: "center" }}>
                     {page === "gamepad" ? (
